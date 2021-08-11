95df31b46d9016d4735443036964a8b91e4bffab
Timo Schuermann <timo.schuermann91@gmx.de>
2021-08-11 11:17:50 +0200

    fix

--- R/shinyNetwork.R
+++ R/shinyNetwork.R
@@ -1,24 +1,24 @@
 shinyNetwork <- function(exb){
   shiny::shinyApp(
-    ui= Shiny::fluidPage(
-      Shiny::fluidRow(DT::dataTableOutput("ExbData")),
-      Shiny::fluidRow(Shiny::selectInput("vars", "Variablen auswählen", choices = names(exb), multiple = TRUE)),
-      Shiny::fluidRow(Shiny::actionButton("plotData", "Auswahl plotten")),
-      Shiny::fluidRow(Shiny::plotOutput("Netzwerk")),
+    ui= shiny::fluidPage(
+      shiny::fluidRow(DT::dataTableOutput("ExbData")),
+      shiny::fluidRow(shiny::selectInput("vars", "Variablen auswählen", choices = names(exb), multiple = TRUE)),
+      shiny::fluidRow(shiny::actionButton("plotData", "Auswahl plotten")),
+      shiny::fluidRow(shiny::plotOutput("Netzwerk")),
       #fluidRow(textOutput("Variablen", "Ausgewählte Variablen")),
-      Shiny::fluidRow(DT::dataTableOutput("clusterData")),
-      Shiny::fluidRow(Shiny::textInput("filename", "Enter filename"),
-                      Shiny::downloadButton("Dcluster", "Download"))
+      shiny::fluidRow(DT::dataTableOutput("clusterData")),
+      shiny::fluidRow(shiny::textInput("filename", "Enter filename"),
+                      shiny::downloadButton("Dcluster", "Download"))
 
     ),
     server = function(input, output){
-      output$ExbData <-  Shiny::renderDataTable({DT::datatable(exb, filter= "top",selection = list(target = 'row', selected= c(seq(1:nrow(exb)))),options = list(lengthChange = TRUE, autoWidth= TRUE, scrollX= TRUE))})
-      NetworkPlot <- Shiny::eventReactive(input$plotData,{
+      output$ExbData <-  shiny::renderDataTable({DT::datatable(exb, filter= "top",selection = list(target = 'row', selected= c(seq(1:nrow(exb)))),options = list(lengthChange = TRUE, autoWidth= TRUE, scrollX= TRUE))})
+      NetworkPlot <- shiny::eventReactive(input$plotData,{
         data <- exb[input$ExbData_rows_selected,]
         Disseration::com_clust(data, variables = input$vars)})
-      output$Netzwerk <- Shiny::renderPlot({plot(NetworkPlot()[[2]],NetworkPlot()[[3]],vertex.size=10, vertex.label.font=20, family="serif", edge.width=igraph::E(NetworkPlot()[[3]])$weight, sub= stringr::str_c("Modularity=",modularity(NetworkPlot()[[2]])))})
-      output$clusterData <- Shiny::renderDataTable({DT::datatable(NetworkPlot()[[1]],options = list(lengthChange = TRUE, autoWidth= TRUE, scrollX= TRUE))})
-      output$Dcluster <- Shiny::downloadHandler(filename = function(){paste(input$filename,".csv", sep="")}, content = function(file){write.csv(NetworkPlot()[[1]], file, row.names = FALSE)})
+      output$Netzwerk <- shiny::renderPlot({plot(NetworkPlot()[[2]],NetworkPlot()[[3]],vertex.size=10, vertex.label.font=20, family="serif", edge.width=igraph::E(NetworkPlot()[[3]])$weight, sub= stringr::str_c("Modularity=",modularity(NetworkPlot()[[2]])))})
+      output$clusterData <- shiny::renderDataTable({DT::datatable(NetworkPlot()[[1]],options = list(lengthChange = TRUE, autoWidth= TRUE, scrollX= TRUE))})
+      output$Dcluster <- shiny::downloadHandler(filename = function(){paste(input$filename,".csv", sep="")}, content = function(file){write.csv(NetworkPlot()[[1]], file, row.names = FALSE)})
 
     }
   )
