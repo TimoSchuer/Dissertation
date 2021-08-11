02c22023489b7fa9c4bd2c910f56b3a00686a367
Timo Schuermann <timo.schuermann91@gmx.de>
2021-08-11 19:47:51 +0200

    fix

--- R/com_clust.R
+++ R/com_clust.R
@@ -2,7 +2,7 @@ com_clust <- function(data,variables = c(1:ncol(data)),weight= 1){
   data[,variables] <- data[,variables] %>% dplyr::mutate(dplyr::across(where(purrr::negate(is.factor)), as.factor))
   adjmatrix <- 1- cluster::daisy(data[,variables], metric = c("gower"), weights = weight) # ungewichteter Vergleich, ausgenommen e0, e1, da nur ein auftreten überhaupt
 
-  x <- matrix::as.matrix(adjmatrix) #schreibt Aehnlichkeitsmatrix in ein R Objekt
+  x <- Matrix::as.matrix(adjmatrix) #schreibt Aehnlichkeitsmatrix in ein R Objekt
 
   g1 <- igraph:::graph_from_adjacency_matrix(x, mode = c("lower"), weighted = TRUE, diag = FALSE, add.colnames = NULL, add.rownames = NA) #erstellt Graphfile aus unterer H??lfte der Aehnlichkeitsmatrix
 
