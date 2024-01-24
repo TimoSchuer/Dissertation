69e59541af109c681dbf802b42e88f64716e845d
Timo Schuermann <timo.schuermann91@gmx.de>
2024-01-24 13:13:47 +0100

    add roxygen sceleton

--- R/sendPraat.R
+++ R/sendPraat.R
@@ -1,3 +1,13 @@
+#' send Praat
+#'
+#' @param data data.frame created bei ExmaraldaR with function readExb has to contain the columns "pathAudio", "Start_time" and "End_time"
+#' @param rowNumber row number of the data frame to be sent to Praat
+#' @param pathPraat path to the folder containing the Praat executable 'sendpraat-win.exe'
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
 sendPraat <- function(data=data.frame(),rowNumber=1, pathPraat=character()){
   stopifnot(length(pathPraat)==1)
   paste(pathPraat,"/sendpraat-win.exe praat ",
